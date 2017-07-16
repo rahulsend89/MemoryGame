65d07f35ab8fb7f93d24de10ab75e828ef344030
Rahul Malik <rahul.send89@gmail.com>
2017-07-16 15:38:51 +0530

    updated sh

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-TEST_CMD="xcodebuild -scheme MemoryGame -workspace MemoryGame.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' build test"
+TEST_CMD="xcodebuild -scheme MemoryGame -workspace MemoryGame.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3.1' build test -enableCodeCoverage YES"
 
-which -s xcpretty
-XCPRETTY_INSTALLED=$?
-
-if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  eval "${TEST_CMD} | xcpretty"
-else
-  eval "$TEST_CMD"
-  fi
+eval "$TEST_CMD"
